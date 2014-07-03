a264eb88b2c58c640208e87f338a45f18c4987b5
nguyen van <nguyenvanvcpt@gmail.com>
2014-07-03 11:26:42 +0700

    new project

---
+++ testpack/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  testpack
+//
+//  Created by nguyen van on 7/3/14.
+//  Copyright (c) 2014 nguyen van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ testpack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testpack
+//
+//  Created by nguyen van on 7/3/14.
+//  Copyright (c) 2014 nguyen van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
