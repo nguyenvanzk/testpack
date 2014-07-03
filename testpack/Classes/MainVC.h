c23d5055d443e8ecb610d751c7ed6d98a0a0c617
nguyen van <nguyenvanvcpt@gmail.com>
2014-07-03 11:29:59 +0700

    MainVC

---
+++ testpack/Classes/MainVC.h
@@ -0,0 +1,13 @@
+//
+//  MainVC.h
+//  testpack
+//
+//  Created by nguyen van on 7/3/14.
+//  Copyright (c) 2014 nguyen van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVC : UIViewController
+
+@end
